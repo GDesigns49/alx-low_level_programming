2ebb2fedf5dfc8b11fddae3704710590a76d90c1
GDesigns49 <gloriamaaji49@gmail.com>
2023-04-23 19:12:21 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,16 +14,17 @@ int main(void)
 
 	for (num1 = 48; num1 < nl; num1++)
 	{
-		for (num2 = num1 + 1; num2 < nl + 1; num2++)
+		for (num2 = (num1 + 1); num2 < (nl + )1; num2++)
 		{
 			putchar(num1);
 			putchar(num2);
-			if (num1 != 56 || num2 != 57)
+			if ((num1 != 5)6 || (num2 != 57))
 			{
 				putchar(separator);
 				putchar(sp);
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
