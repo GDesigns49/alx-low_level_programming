ca946ebd908f149ec1a9d1d6865252d7091a7cac
GDesigns49 <gloriamaaji49@gmail.com>
2023-04-25 14:09:46 -0700

    a logic that prints letters of letters of the alphabet 10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
- * print_alphabet_x10 - prints alphabet 10 times
+* print_alphabet_x10 - prints alphabet 10 times
 */
 void print_alphabet_x10(void)
 {
 	int i;
 	char j;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
