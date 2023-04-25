ca96f2d286ab958c452b314b4fea7226f5280289
GDesigns49 <gloriamaaji49@gmail.com>
2023-04-25 08:45:09 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ */
+void print_alphabet_x10(void)
+
+{
+	int i;
+	int lc;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (lc = 'a'; lc <= 'z'; lc++)
+		{
+			_putchar(lc);
+	_putchar('\n);
+		}
+	}
+}
