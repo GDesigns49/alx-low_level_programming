0d81724d6d33df0b44aedc5150ea37645ad0771c
GDesigns49 <gloriamaaji49@gmail.com>
2023-04-25 10:53:03 -0700

    a logic that prints numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_to_98 - print all numbers from input to 98
+ * print_to_98 - print natural  numbers from n to 98
  * @n: the starting number
  */
 void print_to_98(int n)
 {
-	int print_to_98(int n)
-		if (n >= 98)
-		{
-			while (n >= 98)
-			{
-				printf("%d", n);
-			if(n != 98)
-				printf(",");
-			n--;
-			}
-		}
-		else
-		{
-			while (n <= 98)
-			{
-				printf("%d", n);
-			if (n != 98)
-				n++;
-			}
-		}
-
-			printf("%d\n", n);
+	if (n >= 98)
+	{
+	while (n > 98)
+		printf("%d ", n--);
+	printf("%d\n", n);
+	}
+	else
+	{
+		while (n > 98)
+			printf("%d ", n++);
+		printf("%d\n", n);
+	}
 }
