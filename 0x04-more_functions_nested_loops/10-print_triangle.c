3ef54c9f8c445bcf626ed601dbdb5135d0d840c0
GDesigns49 <gloriamaaji49@gmail.com>
2023-04-27 05:40:37 -0700

    a program that finds and prints the largest prime factor of the number 612852475143, followed by a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,13 +15,13 @@ void print_triangle(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n');
+		_putchar("\n");
 	}
 	else
 	{
 		for (x = 0; x < size; x++)
 		{
-			for (y = size; y > 1; y--)
+			for (y = size - x; y > 1; y--)
 			{
 				_putchar(32);
 			}
@@ -29,7 +29,7 @@ void print_triangle(int size)
 			{
 				_putchar(35);
 			}
-			_putchar('\n');
+			_putchar("\n");
 		}
 	}
 }
