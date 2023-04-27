6aacfaa8c25e4a6662055e8011ec1c005b6ee0cd
GDesigns49 <gloriamaaji49@gmail.com>
2023-04-27 13:44:26 -0700

    more on finctions and nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,13 @@ void more_numbers(void)
 	int x;
 	int y;
 
-	for (x = 0; x < 14; x++)
+	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 10; y++)
+		for (y = 0; y < 14; y++)
 		{
-			if (y > 10)
+			if (y > 9)
 			{
-				_putchar(('y / 10') + '0');
+				_putchar((y / 10) + '0');
 			}
 			else
 			{
