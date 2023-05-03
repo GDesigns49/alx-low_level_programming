fbc4a194e2d5d808ddb2978fbc9fecf7b0ddf482
GDesigns49 <gloriamaaji49@gmail.com>
2023-05-02 18:07:27 -0700

    0x05-pointers_arrays_string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -28,10 +28,13 @@ int _atoi(char *s)
 			d = s[x] - '0';
 			z = z * 10 + d;
 			f = 1;
+			if (s[x + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
 		x++;
 	}
 	if (f == 0)
 		return (0);
-	return (z);
+	return (x);
 }
