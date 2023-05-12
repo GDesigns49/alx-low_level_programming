9bf975136b736c5d5568605ca00bb38c7e6ae05b
GDesigns49 <gloriamaaji49@gmail.com>
2023-05-12 09:02:16 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -31,12 +31,13 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')break;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
 			f = 0;
 		}
 		i++;
 	}
 	if (f == 0)
-		return (0);
-	return (n);
+		return 0;
+	return n;
 }
